e9f023a9f58e2c6b95f08aa85faf2d4b150aa0f1
Oh <osm723@gmail.com>
2024-07-17 20:13:00 +0900

    김영한의 실전 자바 - 중급 2편 섹션 7. 컬렉션 프레임워크 - HashSet 직접 구현하는 Set4 - 제네릭과 인터페이스 도입

---
+++ src/collection/set/member/MemberOnlyHash.java
@@ -0,0 +1,36 @@
+package collection.set.member;
+
+import java.util.Objects;
+
+public class MemberOnlyHash {
+
+    private String id;
+
+    public MemberOnlyHash(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberNoHashNoEq{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+
+//    @Override
+//    public boolean equals(Object object) {
+//        if (this == object) return true;
+//        if (object == null || getClass() != object.getClass()) return false;
+//        MemberOnlyHash that = (MemberOnlyHash) object;
+//        return Objects.equals(id, that.id);
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
